49fba4df4b26544c5b7f22cb7c1d0d1fa311260f
sreesandhya11 <sreesandy1925@gmail.com>
2025-07-10 17:38:26 +0530

    fixtures

--- ride_mgnt/hooks.py
+++ ride_mgnt/hooks.py
@@ -227,6 +227,6 @@ app_license = "mit"
 # 	"Logging DocType Name": 30  # days to retain logs
 # }
 
-# fixture = {
+# fixtures = [
 #     "Customer", "Item", "Ride Booking", "Vehicle Ride"
-# }
+# ]
